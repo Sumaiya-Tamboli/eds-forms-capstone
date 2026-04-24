006c165288ccf53eae14281471cff02711863e8f
Sumaiya-Tamboli <stamboli@adobe.com>
2026-04-24 18:38:27 +0530

    rng prev

--- blocks/form/offer-display.js
+++
@@ -1,6 +0,0 @@
-const range = document.querySelector('.loan-range');
-
-range.addEventListener('input', () => {
-  const value = (range.value - range.min) / (range.max - range.min) * 100;
-  range.style.background = `linear-gradient(to right, #f59e0b ${value}%, #ddd ${value}%)`;
-});
